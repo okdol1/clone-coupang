2a6d3cd966cf2582ffe231b7c911e009b4288c96
Eunbin Yeon <76744586+okdol1@users.noreply.github.com>
2024-01-16 00:42:29 +0900

    [Auth Page] UI 구현 (#1)
    * feat: 전역 스타일 설정
    
    * feat: 로그인 UI 구현
    
    * feat: Loader 컴포넌트 구현
    
    * feat: Input 컴포넌트 구현
    
    * feat: Icon 컴포넌트 구현
    
    * feat: 자동 로그인 컴포넌트 구현
    
    * feat: Divider 컴포넌트 구현
    
    * feat: Button 컴포넌트 구현
    
    * feat: 회원가입 UI
    
    * feat: Reset 컴포넌트 UI

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import styles from "./Heading.module.scss";
 
-const Heading = () => {
-  return (
-    <div>Heading</div>
-  )
+interface IHeadingProps {
+  title: string;
+  subtitle?: string;
+  center?: boolean;
 }
 
-export default Heading
+const Heading = ({ title, subtitle, center }: IHeadingProps) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={center ? styles.center : ""}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.subtitle}>{subtitle}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Heading;
--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import styles from "./Loader.module.scss";
+import { RotatingLines } from "react-loader-spinner";
 
-const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
+interface ILoaderProps {
+  basic?: boolean;
 }
 
-export default Loader
+const Loader = ({ basic }: ILoaderProps) => {
+  if (basic) {
+    return (
+      <div className={styles.basicWrapper}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="30"
+          visible={true}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loader}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="30"
+          visible={true}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Loader;

5b09a8d6dcd523e351e5442cb7b8a066d417e205
okdol1 <okdol0611@gmail.com>
2024-01-16 00:27:06 +0900

    feat: Divider 컴포넌트 구현

--- src/app/(auth)/login/LoginClient.tsx
+++ src/app/(auth)/login/LoginClient.tsx
@@ -8,6 +8,7 @@ import styles from "./Auth.module.scss";
 import Loader from "@/components/loader/Loader";
 import Input from "@/components/input/Input";
 import AutoSignInCheckbox from "@/components/autoSignInCheckbox/AutoSignInCheckbox";
+import Divider from "@/components/divider/Divider";
 
 const LoginClient = () => {
   const [email, setEmail] = useState("");
@@ -70,6 +71,9 @@ const LoginClient = () => {
             <div className={styles.buttonGroup}>
               {/* Button */}
               Button
+              <Divider />
+              Button
+              <Divider />
               <div>
                 {/* Button */}
                 Button

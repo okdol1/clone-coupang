5cec0439fa50b91ac0ec982eacc041b4c061412e
okdol1 <okdol0611@gmail.com>
2024-01-16 00:14:12 +0900

    feat: Input 컴포넌트 구현

--- src/app/(auth)/login/LoginClient.tsx
+++ src/app/(auth)/login/LoginClient.tsx
@@ -5,6 +5,8 @@ import React, { FormEvent, useState } from "react";
 import LogoPath from "@/assets/colorful.svg";
 import { useRouter } from "next/navigation";
 import styles from "./Auth.module.scss";
+import Loader from "@/components/loader/Loader";
+import Input from "@/components/input/Input";
 
 const LoginClient = () => {
   const [email, setEmail] = useState("");
@@ -26,29 +28,53 @@ const LoginClient = () => {
   const signInGoogle = () => {};
 
   return (
-    <section className={styles.page}>
-      <div className={styles.container}>
-        <h1 className={styles.logo}>
-          <Image priority src={LogoPath} alt="logo" />
-        </h1>
-        <form onSubmit={loginUser} className={styles.form}>
-          {/* Input */}
-          Input
-          <div className={styles.group}>
-            {/* 자동 로그인, 비밀번호 수정 */}
-            자동 로그인, 비밀번호 수정
-          </div>
-          <div className={styles.buttonGroup}>
-            {/* Button */}
-            Button
-            <div>
+    <>
+      {isLoading && <Loader />}
+      <section className={styles.page}>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>
+            <Image priority src={LogoPath} alt="logo" />
+          </h1>
+          <form onSubmit={loginUser} className={styles.form}>
+            {/* Input */}
+            <Input
+              email
+              icon="letter"
+              id="email"
+              name="email"
+              label="이메일"
+              placeholder="아이디(이메일)"
+              className={styles.control}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              password
+              icon="lock"
+              id="password"
+              name="password"
+              label="비밀번호"
+              placeholder="비밀번호"
+              className={styles.control}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className={styles.group}>
+              {/* 자동 로그인, 비밀번호 수정 */}
+              자동 로그인, 비밀번호 수정
+            </div>
+            <div className={styles.buttonGroup}>
               {/* Button */}
               Button
+              <div>
+                {/* Button */}
+                Button
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
-    </section>
+          </form>
+        </div>
+      </section>
+    </>
   );
 };
 

0584850f054b0e99e5c29a6af9dbbe84e3084844
okdol1 <okdol0611@gmail.com>
2024-01-29 21:06:41 +0900

    feat: Header 컴포넌트 UI 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ToastProvider from "@/components/toastProvider/ToastProvider";
 import Footer from "@/layouts/footer/Footer";
+import Header from "@/layouts/header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <ToastProvider />
         {children}
         <Footer />
